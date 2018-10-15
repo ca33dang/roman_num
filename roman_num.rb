6ef6dd9dc496628652787125d877614ee2d981df
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-10-15 10:47:20 -0400

    got number 4

--- roman_num.rb
+++ roman_num.rb
@@ -1,11 +1,12 @@
 def romanize(number)
-  letters = %w[I]
-  values = [1]
+  letters = %w[IV I]
+  values = [4, 1]
   roman = letters.zip values
   n = number
   numerals = ""
   roman.each do |v|
     numerals += v[0] * (n/v[1])
+    n = n%v[1] 
   end
   return numerals
 end
