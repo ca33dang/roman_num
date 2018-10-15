2cc7f5c0f1fd7d655ea6b43d3a638fc0441b0593
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-10-15 09:53:59 -0400

    test fail, pair with partner, start new from ground up.

--- romantdd.rb
+++ romantdd.rb
@@ -5,19 +5,7 @@ class Testing < Minitest::Test
 	def test_1_equal_1
 		assert_equal(1, 1)
 	end
-	def test_roman_number
-		assert_equal("XXXX", romanize(40))
-	end
-	def test_1
-		assert_equal("LXV", romanize(65))
-	end
-	def test_2_class
-		assert_equal(String, romanize(11).class)
-	end
-	def test_3_length
-		assert_equal(2, romanize(20).length)
-	end
-	def test_4_num_convert
-		assert_equal("MMXVIII", romanize(2018))
+	def test_for_string
+		assert_equal(String, romanize(0).class)
 	end
 end
