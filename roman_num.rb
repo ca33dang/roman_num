df2922dcd5c3bd18fa95a47dad7d860486be0f56
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-10-15 10:36:40 -0400

    pass test array

--- roman_num.rb
+++ roman_num.rb
@@ -1,3 +1,11 @@
 def romanize(number)
-  "I"
+  letters = %w[I]
+  values = [1]
+  roman = letters.zip values
+  n = number
+  numerals = ""
+  roman.each do |v|
+    numerals += v[0] * (n/v[1])
+  end
+  return numerals
 end
